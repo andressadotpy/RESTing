7de7aac765d58fb54315e892baf058db7c1cd0e1
andressa <acabistani@gmail.com>
2020-10-08 16:24:45 -0300

    Changed forms; changed new_company and new_worker views

--- companies/urls.py
+++ companies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import new_company
+from .views import new_company, new_worker
 
 
 urlpatterns = [
     path('new_company/', new_company, name='new_company'),
+    path('new_worker/', new_worker, name='new_worker'),
 ]
 
--- companies/views.py
+++ companies/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from .models import Company, Worker
 
 
 def new_company(request):
+
+    if request.POST:
+        name = request.POST['name']
+        cnpj = request.POST['cnpj']
+        company = Company.objects.create(name=name, cnpj=cnpj)
+        company.save()
     return render(request, 'new_company.html')
+
+
+def new_worker(request):
+    all_companies = Company.objects.all()
+
+    companies = {
+        'companies': all_companies 
+    }
+
+    if request.POST:
+        name = request.POST['name']
+        cpf = request.POST['cpf']
+        address = request.POST['address']
+        job_description = request.POST['job_description']
+        companies = request.POST['companies']
+        worker = Worker.objects.create(name=name, cpf=cpf, address=address, job_description=job_description, companies=companies)
+        worker.save()
+        return redirect('index')
+
+    return render(request, 'new_worker.html', companies)
