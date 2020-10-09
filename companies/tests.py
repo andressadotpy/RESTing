ceb7a5df25f7febc587e6f9cbc089235c6d9377c
andressa <acabistani@gmail.com>
2020-10-09 12:08:01 -0300

    Added test to Worker; found solution to bug

--- companies/tests.py
+++ companies/tests.py
@@ -21,3 +21,30 @@ class TestCompanyModel(TestCase):
         company = Company.objects.get(id=1)
         expected_cnpj = company.cnpj
         self.assertEquals(expected_cnpj, 12345)    
+
+
+class TestWorkerModel(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        companies = [
+            Company.objects.create(name='Company Test 1', cnpj=12345),
+            Company.objects.create(name='Company Test 2', cnpj=67891)
+        ]
+        worker = Worker.objects.create(name='Worker Test',
+                            cpf=123456789, address='Address', job_description='This is the job description')
+        
+        worker.companies.set(companies)
+
+
+    def test_if_worker_name_cpf_address_and_job_description_are_right(self):
+        worker = Worker.objects.get(id=1)
+        expected_name = f'{worker.name}'
+        expected_cpf = worker.cpf
+        expected_address = f'{worker.address}'
+        expected_job_description = f'{worker.job_description}'
+        self.assertEquals(expected_name, 'Worker Test')
+        self.assertEquals(expected_cpf, 123456789)
+        self.assertEquals(expected_address, 'Address')
+        self.assertEquals(expected_job_description, 'This is the job description')
+        
