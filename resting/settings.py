530a042e6a125694b14f1ad73acf15d43c74b68e
andressa <acabistani@gmail.com>
2020-10-09 18:35:37 -0300

    Added whitenoise

--- resting/settings.py
+++ resting/settings.py
@@ -57,6 +57,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'resting.urls'
